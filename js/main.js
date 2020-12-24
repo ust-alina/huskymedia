4433396f67eaaeeb2633f9f215793b3389543a3c
ust-alina <66687007+ust-alina@users.noreply.github.com>
2020-12-24 16:52:00 +0300

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,96 @@
+$(document).ready(function(){
+
+    $('.slider').slick({
+        dots: false,
+        slidesToShow: 3,
+        speed: 1000,
+        responsive:[
+			{
+				breakpoint: 992,
+				settings: {
+					slidesToShow:2
+				}
+			},	
+			{
+				breakpoint: 768,
+				settings: {
+					slidesToShow:2
+				}
+			},
+			{
+				breakpoint: 550,
+				settings: {
+					slidesToShow:1,
+					arrows : false
+				}
+			}
+		]
+        
+	});
+
+	$('.slider2').slick({
+        dots: false,
+        slidesToShow: 4,
+		speed: 1000,
+		slidesToScroll: 1,
+        autoplay: true,
+        autoplaySpeed: 2000,
+        responsive:[
+			{
+				breakpoint: 992,
+				settings: {
+					slidesToShow:3
+				}
+			},	
+			{
+				breakpoint: 768,
+				settings: {
+					slidesToShow:2
+				}
+			},
+			{
+				breakpoint: 550,
+				settings: {
+					slidesToShow:1,
+					arrows : false
+				}
+			}
+		]
+        
+	});
+
+	
+
+
+});
+
+$(function() {
+	let header = $('.header');
+	 
+	$(window).scroll(function() {
+	  if($(this).scrollTop() > 1) {
+	   header.addClass('header_fixed');
+	  } else {
+	   header.removeClass('header_fixed');
+	  }
+	});
+   });
+
+   $(function() {
+	let header = $('.header');
+	let hederHeight = header.height(); // вычисляем высоту шапки
+	 
+	$(window).scroll(function() {
+	  if($(this).scrollTop() > 1) {
+	   header.addClass('header_fixed');
+	   $('main').css({
+		  'paddingTop': hederHeight+'px' // делаем отступ у body, равный высоте шапки
+	   });
+	  } else {
+	   header.removeClass('header_fixed');
+	   $('main').css({
+		'paddingTop': 0 // удаляю отступ у body, равный высоте шапки
+	   })
+	  }
+	});
+   });
